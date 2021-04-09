6c2d3ba82df9c8b171f0464e43f9c6519bf1954f
unknown <guiconci@hotmail.com>
2021-04-09 18:07:08 +0700

    'app-server'

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+import dash
+from dash.dependencies import Input, Output
+from dash_html_components.Div import Div
+import dash_table
+import dash_core_components as dcc
+import dash_html_components as html
+import pandas as pd
+import numpy as np
+
+app = dash.Dash(__name__)
+server = app.server
+
+
 print('Trial')
