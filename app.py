0061942c39116ea10a9497cc758c49092cf3f22c
unknown <guiconci@hotmail.com>
2021-04-09 18:37:46 +0700

    'test'

--- app.py
+++ app.py
@@ -10,8 +10,10 @@ import numpy as np
 app = dash.Dash(__name__)
 server = app.server
 
+df= [2,3,4,5]
 
-app.layout = html.Div(html.H1(id='Title1',children='2.46 mm'))
+
+app.layout = html.Div(html.H1(id='Title1',children=df))
 
 
 if __name__ == '__main__':
